fe425ed8e56ddc266a42d9fe73cb14b73d08beb9
Nick <tangerinefr0g@hotmail.com>
2026-01-30 21:24:03 +0700

    add server timeout configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -23,6 +23,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not load .env file: %v", err)
 	}
+	
+	addr := os.Getenv("SRV_ADDR")
+	if addr == "" {
+		log.Fatalf("Server address is not defined in the .env file")
+	}
 
 	db, err := data.Init()
 	if err != nil {
@@ -38,28 +43,12 @@ func main() {
 		log.Fatalf("Config init error: %v", err)
 	}
 
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.Use(cors.Default())
-
-	h := handlers.NewHandler(db)
-	h.SetupRoutes(r)
-
-	addr := os.Getenv("SRV_ADDR")
-	if addr == "" {
-		log.Fatalf("Server address is not defined in the .env file")
-	}
-
-	go startRecurrentJobs(ctx, db)
-
-	srv := &http.Server{
-		Addr:    addr,
-		Handler: r,
-	}
+	srv := configureServer(ctx, addr, db)
 
 	go func() {
 		log.Printf("Server is listening on '%s'...\n", addr)
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
 			log.Fatalf("Server error: %v", err)
 		}
 	}()
@@ -77,6 +66,25 @@ func main() {
 	srv.Shutdown(shutdownCtx)
 }
 
+func configureServer(ctx context.Context, addr string, db *data.DB) *http.Server {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	h := handlers.NewHandler(db)
+	h.SetupRoutes(r)
+
+	go startRecurrentJobs(ctx, db)
+
+	return &http.Server{
+		Addr:         addr,
+		Handler:      r,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+}
+
 func initConfig(ctx context.Context, db *data.DB) error {
 	configRepo := repositories.NewConfigRepo(db)
 	err := configRepo.Init(ctx)
@@ -90,8 +98,5 @@ func initConfig(ctx context.Context, db *data.DB) error {
 
 func startRecurrentJobs(ctx context.Context, db *data.DB) {
 	periodHour := 1 * time.Hour
-	err := scheduler.ScrapeRecurring(ctx, periodHour, db)
-	if err != nil {
-		log.Printf("Recurrent scraping error: %v", err)
-	}
+	scheduler.ScrapeRecurring(ctx, periodHour, db)
 }
