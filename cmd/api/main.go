4edb8a90f01367cff81c524f01f9eb4f159561e7
Nick <tangerinefr0g@hotmail.com>
2026-01-30 20:24:51 +0700

    add graceful server shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,7 +7,10 @@ import (
 	"job-scraper/internal/handlers"
 	"job-scraper/internal/services/scheduler"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -25,6 +28,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.Close()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -48,9 +52,29 @@ func main() {
 
 	go startRecurrentJobs(ctx, db)
 
-	log.Printf("Server is listening on ':%s'...\n", addr)
-	err = r.Run(addr)
-	log.Printf("Server error: %v\n", err)
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+
+	go func() {
+		log.Printf("Server is listening on '%s'...\n", addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutting down server...")
+	cancel()
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	srv.Shutdown(shutdownCtx)
 }
 
 func initConfig(ctx context.Context, db *data.DB) error {
